b47ee09f92b92eee3232b4ea41238565612ef32e
Roni Juppi <Roni Juppi>
2020-08-07 17:54:11 +0300

    Cleanup and hide some bigger implementations in .cpp files

--- src/Renderer.hpp
+++ src/Renderer.hpp
@@ -9,6 +9,25 @@
 #include <vector>
 
 class Renderer {
+public:
+    explicit Renderer(SDL_Window *window);
+    ~Renderer() noexcept;
+
+    Renderer(const Renderer &) = delete;
+    Renderer &operator=(const Renderer &) = delete;
+    Renderer(Renderer &&) = delete;
+    Renderer &operator=(Renderer &&) = delete;
+
+    void clear(uint8_t r, uint8_t g, uint8_t b) const noexcept;
+    void present() const noexcept;
+    void drawText(const char *text, int x, int y, float scale = 1.f) noexcept;
+
+private:
+    static void setSurfacePixelColor(
+        SDL_Surface *surface, int x, int y, uint8_t r, uint8_t g, uint8_t b, uint8_t a) noexcept;
+
+    static constexpr int DefaultFontPixelSize = 24;
+
     SDL_Renderer *m_sdlRenderer;
 
     struct FontInfo {
@@ -24,124 +43,4 @@ class Renderer {
         SDL_Texture *currentTexture {};
     };
     FontInfo m_font;
-
-public:
-    static constexpr int DefaultFontPixelSize = 24;
-
-    explicit Renderer(SDL_Window *window)
-    {
-        m_sdlRenderer
-            = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-        if (!m_sdlRenderer) {
-            throw std::runtime_error(
-                fmt::format("Failed to initialize SDL_Renderer: {}", SDL_GetError()));
-        }
-
-        m_font.currentData = readEntireBinaryFile("Roboto-Regular.ttf");
-        if (m_font.currentData.empty()) {
-            throw std::runtime_error("Failed to initialize SDL_Renderer");
-        }
-
-        m_font.bufferWidth = 512;
-        m_font.bufferHeight = 512;
-        m_font.pixels.reserve(m_font.bufferWidth * m_font.bufferHeight * sizeof(m_font.pixels[0]));
-        m_font.charData.resize(512);
-        // TODO: Use Improved 3D API (stbtt_PackBegin, etc, maybe even stb_rect_pack.h)
-        const int bakeFontResult = stbtt_BakeFontBitmap(m_font.currentData.data(), 0,
-            FontInfo::BufferPixelSize, m_font.pixels.data(), m_font.bufferWidth,
-            m_font.bufferHeight, 0, m_font.charData.size(), m_font.charData.data());
-
-        if (bakeFontResult > 0) { // unused rows
-        } else { // fit this many characters (abs)
-            m_font.charData.resize(std::abs(bakeFontResult));
-        }
-
-        // Init font texture
-        {
-            int w = m_font.bufferWidth;
-            int h = m_font.bufferHeight;
-
-            SDL_Surface *surface = SDL_CreateRGBSurface(0, w, h, 32, 0, 0, 0, 0);
-            if (!surface) {
-                throw std::runtime_error(
-                    fmt::format("Failed to create SDL_Surface: {}\n", SDL_GetError()));
-            }
-
-            SDL_LockSurface(surface);
-            for (int i = 0; i < surface->w && surface; ++i) {
-                for (int j = 0; j < surface->h; ++j) {
-                    uint8_t alpha = m_font.pixels[j * surface->w + i];
-                    setSurfacePixelColor(surface, i, j, alpha, alpha, alpha, alpha);
-                }
-            }
-            SDL_UnlockSurface(surface);
-
-            m_font.currentTexture = SDL_CreateTextureFromSurface(m_sdlRenderer, surface);
-            if (!m_font.currentTexture) {
-                throw std::runtime_error(
-                    fmt::format("Failed to create SDL_Texture: {}\n", SDL_GetError()));
-            }
-
-            SDL_SetTextureBlendMode(m_font.currentTexture, SDL_BLENDMODE_ADD);
-        }
-    }
-
-    ~Renderer() noexcept
-    {
-        if (m_sdlRenderer) {
-            SDL_DestroyRenderer(m_sdlRenderer);
-        }
-    }
-
-    Renderer(const Renderer &) = delete;
-    Renderer &operator=(const Renderer &) = delete;
-    Renderer(Renderer &&) = delete;
-    Renderer &operator=(Renderer &&) = delete;
-
-    void clear(uint8_t r, uint8_t g, uint8_t b) const noexcept
-    {
-        SDL_SetRenderDrawColor(m_sdlRenderer, r, g, b, 255);
-        SDL_RenderClear(m_sdlRenderer);
-    }
-
-    void present() const noexcept { SDL_RenderPresent(m_sdlRenderer); }
-
-    void drawText(const char *text, int x, int y, float scale = 1.f) noexcept
-    {
-#if 0
-        SDL_Rect fontTexRect;
-        fontTexRect.x = 720;
-        fontTexRect.y = 70;
-        fontTexRect.h = 512;
-        fontTexRect.w = 512;
-        SDL_RenderCopy(m_sdlRenderer, m_font.currentTexture, nullptr, &fontTexRect);
-#endif
-
-        for (int ch = 0; text[ch] != '\0'; ++ch) {
-            const stbtt_bakedchar &bc = m_font.charData[text[ch]];
-
-            const int w = bc.x1 - bc.x0;
-            const int h = bc.y1 - bc.y0;
-            SDL_Rect src { bc.x0, bc.y0, w, h };
-            SDL_Rect dst {
-                static_cast<int>(static_cast<float>(x) + bc.xoff * scale),
-                static_cast<int>(static_cast<float>(y) + bc.yoff * scale),
-                static_cast<int>(static_cast<float>(w) * scale),
-                static_cast<int>(static_cast<float>(h) * scale),
-            };
-            SDL_RenderCopy(m_sdlRenderer, m_font.currentTexture, &src, &dst);
-
-            x += static_cast<int>(bc.xadvance * scale);
-        }
-    }
-
-private:
-    static void setSurfacePixelColor(
-        SDL_Surface *surface, int x, int y, uint8_t r, uint8_t g, uint8_t b, uint8_t a) noexcept
-    {
-        int off = y * surface->w + x;
-        uint32_t *ptr = (static_cast<uint32_t *>(surface->pixels)) + off;
-        uint32_t color = SDL_MapRGBA(surface->format, r, g, b, a);
-        *ptr = color;
-    }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,87 @@
 #include "Renderer.hpp"
 #include "SDL.h"
+#include "SDL_video.h"
 #include "Utils.hpp"
 #include <fmt/format.h>
 
-#define STB_TRUETYPE_IMPLEMENTATION
-#include "stb_truetype.h"
-
 class App {
-    static constexpr int WIDTH = 1280;
-    static constexpr int HEIGHT = 720;
-
-    SDL_Window *m_window { [] {
-        if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-            throw std::runtime_error(fmt::format("Failed to init SDL {}\n", SDL_GetError()));
-        }
-        SDL_Window *window = SDL_CreateWindow(
-            "rojuide", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, 0);
-
-        if (!window) {
-            throw std::runtime_error(fmt::format("Failed to create window: {}\n", SDL_GetError()));
-        }
-        return window;
-    }() };
-    Renderer m_renderer { m_window };
-
 public:
-    App() = default;
-    ~App() noexcept
-    {
-        SDL_DestroyWindow(m_window);
-        SDL_Quit();
-    }
+    App();
+    ~App() noexcept;
 
     App(const App &) = delete;
     App &operator=(const App &) = delete;
     App(App &&) = delete;
     App &operator=(App &&) = delete;
 
-    void run()
-    {
-        bool quit = false;
-        while (!quit) {
-            SDL_Event sdlEvent;
-            while (SDL_PollEvent(&sdlEvent)) {
-                uint32_t type = sdlEvent.type;
-                switch (type) {
-                case SDL_QUIT: {
-                    quit = true;
-                } break;
+    void run();
 
-                case SDL_KEYDOWN: {
-                    if (sdlEvent.key.keysym.sym == SDLK_ESCAPE) {
-                        quit = true;
-                    }
-                } break;
+private:
+    static constexpr int WIDTH = 1280;
+    static constexpr int HEIGHT = 720;
 
-                default:
-                    break;
-                }
-            }
+    static SDL_Window *initSDLWindow();
 
-            m_renderer.clear(91, 0, 176);
-
-            m_renderer.drawText("Heljo world!", 35, 200, 2);
-            m_renderer.drawText("Wassup world!", 35, 250, 0.35);
-            m_renderer.drawText("Heljo world!", 35, 350, 0.82);
-            m_renderer.drawText("Wassup world!", 35, 400, 0.83);
-            m_renderer.drawText("Wazzzaaaa!", 35, 500, 0.15);
-            m_renderer.drawText("Larger text?", WIDTH / 2 - 100, HEIGHT / 2, 5.331);
-
-            m_renderer.present();
-        }
-    }
+    SDL_Window *m_window { initSDLWindow() };
+    Renderer m_renderer { m_window };
 };
+
+App::App() = default;
+App::~App() noexcept
+{
+    SDL_DestroyWindow(m_window);
+    SDL_Quit();
+}
+
+void App::run()
+{
+    bool quit = false;
+    while (!quit) {
+        SDL_Event sdlEvent;
+        while (SDL_PollEvent(&sdlEvent)) {
+            uint32_t type = sdlEvent.type;
+            switch (type) {
+            case SDL_QUIT: {
+                quit = true;
+            } break;
+
+            case SDL_KEYDOWN: {
+                if (sdlEvent.key.keysym.sym == SDLK_ESCAPE) {
+                    quit = true;
+                }
+            } break;
+
+            default:
+                break;
+            }
+        }
+
+        m_renderer.clear(91, 0, 176);
+
+        m_renderer.drawText("Heljo world!", 35, 200, 2);
+        m_renderer.drawText("Wassup world!", 35, 250, 0.35);
+        m_renderer.drawText("Heljo world!", 35, 350, 0.82);
+        m_renderer.drawText("Wassup world!", 35, 400, 0.83);
+        m_renderer.drawText("Wazzzaaaa!", 35, 500, 0.15);
+        m_renderer.drawText("Larger text?", WIDTH / 2 - 100, HEIGHT / 2, 5.331);
+
+        m_renderer.present();
+    }
+}
+
+SDL_Window *App::initSDLWindow()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        throw std::runtime_error(fmt::format("Failed to init SDL {}\n", SDL_GetError()));
+    }
+    SDL_Window *window = SDL_CreateWindow(
+        "rojuide", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, 0);
+
+    if (!window) {
+        throw std::runtime_error(fmt::format("Failed to create window: {}\n", SDL_GetError()));
+    }
+    return window;
+}
 
 int main(int /*argc*/, char ** /*argv*/)
 {
